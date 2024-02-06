be64fcc052eda8d2aa87a4cc6516d28e88c72900
Gustaf Ekström <gusek93@gmail.com>
2024-02-06 20:23:21 +0100

    Add get request for data, and display of data

--- recipe-frontend/src/App.tsx
+++ recipe-frontend/src/App.tsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 import "./App.css";
 
 function App() {
+  const [myRecipes, setMyRecipes] = useState([]);
+  useEffect(() => {
+    const fetchRecipes = async () => {
+      const response = await axios.get("http://localhost:3000/api/recipes");
+      setMyRecipes(response.data);
+    };
+    fetchRecipes();
+  }, []);
   return (
     <div className="app">
       <h2>My Recipes</h2>
+      {myRecipes}
     </div>
   );
 }
